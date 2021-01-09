5c65c0febbeab27404be4f7642bf50b2fab1b1c5
Nitish Raju <nitishraju@outlook.com>
2021-01-09 02:26:51 -0500

    Solved Ex 6.3

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { voteFor } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -7,6 +8,7 @@ const App = () => {
 
   const vote = (id) => {
     console.log('vote', id)
+    dispatch(voteFor(id))
   }
 
   return (
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,23 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
+  if (action.type === 'VOTE') {
+    const newArray = state.map(anecdote => 
+      anecdote.id !== action.data.id 
+        ? anecdote 
+        : {...anecdote, votes: anecdote.votes + 1} 
+    )
+    return newArray
+  }
 
   return state
+}
+
+export const voteFor = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
 }
 
 export default reducer
